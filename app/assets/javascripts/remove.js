f67990e58fd7f89e79fb701d797b229c41702522
Chris Barrett <chris.barrett@defra.gov.uk>
2019-11-18 17:05:54 +0000

    Added add and remove activities functions

---
+++ app/assets/javascripts/remove.js
@@ -0,0 +1,5 @@
+/* global $ */
+
+function delete_element (activity) { // This will be called on click and passes the activity
+ $.post('/remove' +activity) // This calls the server.
+window.location.reload();  }
--- app/routes.js
+++ app/routes.js
@@ -3,5 +3,30 @@ const router = express.Router()
 
 
 
+//add another activity to the activities list
+router.post('/types-of-waste', function (req, res) {
+      let anotherActivity = req.session.data['another-activity']
+      let activities = req.session.data['waste-activities']
+      let newActivities = req.session.data['waste-activities-another']
+      if ( anotherActivity == 'true') {
+        activities.push(newActivities)
+        res.redirect('activities-summary')
+      } else {
+        res.redirect('types-of-waste')
+      }
+})
+
+
+//remove activity from activities list
+router.post("/remove:activity", function (req, res) {
+    let activities = req.session.data['waste-activities']
+    let activity = req.params.activity
+    var deleteName = activities.indexOf(activity);
+    activities.splice(deleteName, 1);
+    res.redirect('activities-summary');
+})
+
+
+
 
 module.exports = router
